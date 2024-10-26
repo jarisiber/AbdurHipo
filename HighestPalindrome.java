2920fca88721ac4c9ee518e46fad985e3e36e19d
Abdu <140783112+jarisiber@users.noreply.github.com>
2024-10-26 23:11:06 +0700

    Add files via upload

---
+++ HighestPalindrome.java
@@ -0,0 +1,74 @@
+package hitopia;
+
+import java.text.ParseException;
+
+public class HighestPalindrome {
+
+	static String ChangePalinMax(String str, int k)
+	{
+		char palin[] = str.toCharArray();
+		String ans = "";
+
+		int l = 0;
+		int r = str.length() - 1;
+
+		while (l < r) {
+			if (str.charAt(l) != str.charAt(r)) {
+				palin[l] = palin[r] = (char)Math.max(
+				str.charAt(l), str.charAt(r));
+				k--;
+			}
+			l++;
+			r--;
+		}
+
+		if (k < 0) {
+			return "Not possible";
+		}
+
+		l = 0;
+		r = str.length() - 1;
+
+		while (l <= r) {
+
+			if (l == r) {
+				if (k > 0) {
+					palin[l] = '9';
+				}
+			}
+
+			if (palin[l] < '9') {
+
+				if (k >= 2 && palin[l] == str.charAt(l)
+					&& palin[r] == str.charAt(r)) 
+				{
+					k -= 2;
+					palin[l] = palin[r] = '9';
+				} else if (k >= 1
+						&& (palin[l] != str.charAt(l)
+						|| palin[r]
+								!= str.charAt(r))) 
+					{
+						k--;
+						palin[l] = palin[r] = '9';
+					}
+			}
+		
+			l++;
+			r--;
+		}
+		for (int i = 0; i < palin.length; i++)
+		ans += palin[i];
+		return ans;
+	}
+
+
+	public static void main(String[] args) 
+		{
+		String str = "3943";
+		int k = 1;
+		System.out.println(
+				ChangePalinMax(str, k));
+		}
+}
+
